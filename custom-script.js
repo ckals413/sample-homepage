becca24b0f1376cca871eae05f53e3e2b55ade75
ckals413 <ckals413@naver.com>
2023-10-04 23:35:28 +0900

    Modify button listener

--- custom-script.js
+++ custom-script.js
@@ -1,20 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // 버튼 클릭 시 알림 메시지 표시
-    const alertButton = document.getElementById("alert-button");
-    alertButton.addEventListener("click", function() {
+document.addEventListener("DOMContentLoaded", function () {
+  // 버튼 클릭 시 알림 메시지 표시
+  const alertButton = document.getElementById("alert-button");
+  /*  alertButton.addEventListener("click", function() {
         alert("Button clicked!");
-    });
+    }); */
 
-    // 이미지 변경
-    const imageElement = document.getElementById("change-image");
-    const originalImageSrc = imageElement.src;
-    const newImageSrc = "./images/mountain.jpg";
+  // "Learn more" 버튼 클릭 시 learn.html로 이동
+  alertButton.addEventListener("click", function () {
+    window.location.href = "learn-more.html";
+  });
 
-    imageElement.addEventListener("mouseover", function() {
-        imageElement.src = newImageSrc;
-    });
+  // 이미지 변경
+  const imageElement = document.getElementById("change-image");
+  const originalImageSrc = imageElement.src;
+  const newImageSrc = "./images/mountain.jpg";
 
-    imageElement.addEventListener("mouseout", function() {
-        imageElement.src = originalImageSrc;
-    });
+  imageElement.addEventListener("mouseover", function () {
+    imageElement.src = newImageSrc;
+  });
+
+  imageElement.addEventListener("mouseout", function () {
+    imageElement.src = originalImageSrc;
+  });
 });
